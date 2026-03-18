69307c9ec83f19116215000e5b2ebcca4b123963
Kim Seunjae <rlatmdwo9@gmail.com>
2026-03-18 13:59:37 +0900

    구조 1

---
+++ backend/src/main/java/com/kcmc/nexuraT/backend/domain/hello/controller.java
@@ -0,0 +1,13 @@
+package com.kcmc.nexuraT.backend.domain.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello";
+    }
+}
